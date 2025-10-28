cb7fc595e6f380401fc997945a592d1f68cf7825
asyakandyba <asyakan03@gmail.com>
2025-10-28 20:06:36 +0200

    Add function that swaps balls css

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ function onBallClick(elBall, maxDiameter) {
     const randColor = getRandomColor()
     elBall.style.backgroundColor = randColor
 }
+
+function onBallSwap(){
+    console.log('hi:')
+    const elBall = document.querySelector('.ball')
+    const elAnotherBall = document.querySelector('.another-ball')
+
+    const ballColor =  window.getComputedStyle(elBall).backgroundColor
+    const anotherBallColor = window.getComputedStyle(elAnotherBall).backgroundColor
+
+    elBall.style.backgroundColor = anotherBallColor
+    elAnotherBall.style.backgroundColor = ballColor
+
+    const ballWidth =  window.getComputedStyle(elBall).width
+    const anotherBallwidth = window.getComputedStyle(elAnotherBall).width
+
+    elBall.style.width = anotherBallwidth
+    elAnotherBall.style.width = ballWidth
+
+    elBall.style.height = anotherBallwidth
+    elAnotherBall.style.height = ballWidth
+
+
+}
