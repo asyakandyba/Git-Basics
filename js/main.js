1741cc4db15c37dd8fa9f4d98c9d81a55f61f342
asyakandyba <asyakan03@gmail.com>
2025-10-28 15:22:39 +0200

    Limit the ball size

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ var gSize = 100
 
 function onBallClick(elBall) {
     gSize += 50
+    if (gSize > 400) gSize = 100
     elBall.style.width = gSize + 'px'
     elBall.style.height = gSize + 'px'
+
 }
