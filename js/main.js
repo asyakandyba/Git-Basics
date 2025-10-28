a6cb65715d68704701eafc8089f1562e6b10c591
asyakandyba <asyakan03@gmail.com>
2025-10-28 20:28:57 +0200

    Add function reduce baal diameter

--- js/main.js
+++ js/main.js
@@ -7,30 +7,44 @@ function onBallClick(elBall, maxDiameter) {
     if (gSize > maxDiameter) gSize = 100
     elBall.style.width = gSize + 'px'
     elBall.style.height = gSize + 'px'
-    
+
     const randColor = getRandomColor()
     elBall.style.backgroundColor = randColor
 }
 
-function onBallSwap(){
-    console.log('hi:')
+function onBallSwap() {
     const elBall = document.querySelector('.ball')
     const elAnotherBall = document.querySelector('.another-ball')
 
-    const ballColor =  window.getComputedStyle(elBall).backgroundColor
+    const ballColor = window.getComputedStyle(elBall).backgroundColor
     const anotherBallColor = window.getComputedStyle(elAnotherBall).backgroundColor
 
     elBall.style.backgroundColor = anotherBallColor
     elAnotherBall.style.backgroundColor = ballColor
 
-    const ballWidth =  window.getComputedStyle(elBall).width
-    const anotherBallwidth = window.getComputedStyle(elAnotherBall).width
+    const ballSize = window.getComputedStyle(elBall).width
+    const anotherBallSize = window.getComputedStyle(elAnotherBall).width
 
-    elBall.style.width = anotherBallwidth
-    elAnotherBall.style.width = ballWidth
+    elBall.style.width = anotherBallSize
+    elAnotherBall.style.width = ballSize
 
-    elBall.style.height = anotherBallwidth
-    elAnotherBall.style.height = ballWidth
+    elBall.style.height = anotherBallSize
+    elAnotherBall.style.height = ballSize
+}
 
+function onBallReduceDiameter() {
+    const elBall = document.querySelector('.ball')
+    const elAnotherBall = document.querySelector('.another-ball')
 
+    var ballSize = elBall.getBoundingClientRect().width - getRandomInt(20, 60)
+    var anotherBallSize = elAnotherBall.getBoundingClientRect().width - getRandomInt(20, 60)
+
+    if (ballSize < 100) ballSize = 100
+    if (anotherBallSize < 100) anotherBallSize = 100
+
+    elBall.style.width = ballSize + 'px'
+    elAnotherBall.style.width = anotherBallSize + 'px'
+
+    elBall.style.height = ballSize + 'px'
+    elAnotherBall.style.height = anotherBallSize + 'px'
 }
