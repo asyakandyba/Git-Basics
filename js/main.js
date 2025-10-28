6fddc2187f42419164d92b8cde1e6de695108a2c
asyakandyba <asyakan03@gmail.com>
2025-10-28 19:36:04 +0200

    Add max diameter for balls to event handler

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 var gSize = 100
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
     gSize += getRandomInt(20, 60)
-    if (gSize > 400) gSize = 100
+    if (gSize > maxDiameter) gSize = 100
     elBall.style.width = gSize + 'px'
     elBall.style.height = gSize + 'px'
     
