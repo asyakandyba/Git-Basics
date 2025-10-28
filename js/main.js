dc881ff3c544666b9f3bc479249d282ad0863dfe
asyakandyba <asyakan03@gmail.com>
2025-10-28 16:19:47 +0200

    Clean up

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ var gSize = 100
 
 function onBallClick(elBall) {
     gSize += getRandomInt(20, 60)
-    console.log('gSize:', gSize)
     if (gSize > 400) gSize = 100
     elBall.style.width = gSize + 'px'
     elBall.style.height = gSize + 'px'
+    
     const randColor = getRandomColor()
     elBall.style.backgroundColor = randColor
 }
